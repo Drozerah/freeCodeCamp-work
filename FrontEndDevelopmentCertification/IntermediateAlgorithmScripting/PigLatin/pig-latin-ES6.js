287408e5f180072d5037946799aa1e4cc7e6d2b8
Drozerah <drozerah@drozerah.user>
2018-03-09 14:36:45 +0100

    Addes Pig Latin chalenge

---
+++ FrontEndDevelopmentCertification/IntermediateAlgorithmScripting/PigLatin/pig-latin-ES6.js
@@ -0,0 +1,69 @@
+/** 
+ *  Pig Latin
+ *      Translate the provided string to pig latin.
+ *      Pig Latin takes the first consonant (or consonant cluster) of an English word, moves it to the end of the word and suffixes an "ay".
+ *      If a word begins with a vowel you just add "way" to the end.
+ *      Input strings are guaranteed to be English words in all lowercase. 
+ *    
+ *  @see {@link https://www.freecodecamp.org/challenges/pig-latin}
+ * 
+ *  @see {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split}
+ *  @see {@link https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/Array/splice}
+ *  @see {@link https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/Array/filter}
+ *  @see {@link https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Instructions/do...while}
+ * 
+ *  Coded by Drozerah :
+ *      @see {@link https://www.freecodecamp.org/drozerah}
+ * 
+ *  Coded time : 09/03/2018
+ * 
+ *  GitHub repository : 
+ *      @see {@link https://github.com/Drozerah/freeCodeCamp-work/blob/master/FrontEndDevelopmentCertification/IntermediateAlgorithmScripting/PigLatin/pig-latin.js}
+ * 
+ */
+const translatePigLatin = str => {
+
+    // split string characters into an array
+    let splits = str.split('');
+
+    let diagram = [];
+    const ay = "ay";
+    const way = "way";
+
+    // sort characters like consonants or vowels and construct a 1/0 representation of the filtered string
+    const filtered = splits.filter(letter => {
+
+        if (letter == "a" || letter == "e" || letter == "i" || letter == "o" || letter == "u") {
+            diagram.push(1);
+        } else {
+            diagram.push(0);
+        }
+
+    });
+
+    // if the firsts character strings are consonants
+    if (diagram[0] == 0) {
+        let i = 0;
+        do {
+            // push first consonant or consonant cluster caracters to the end of the split word
+            splits.push(splits[i]);
+            // remove it/them from their firsts positions
+            splits.splice(i, 1, '');
+            i += 1;
+        } while (diagram[i] < 1);
+        // add ay to the end
+        console.log("splits =>", splits.join('') + ay);
+        return splits.join('') + ay;
+    }
+    // else, if the word begins with a vowel, add way to the end
+    console.log("else =>", splits.join('') + way);
+    return splits.join('') + way;
+
+    //return str;
+}
+
+translatePigLatin("california") //should return "aliforniacay".
+translatePigLatin("paragraphs") //should return "aragraphspay".
+translatePigLatin("glove") //should return "oveglay".
+translatePigLatin("algorithm") //should return "algorithmway".
+translatePigLatin("eight") //should return "eightway".
